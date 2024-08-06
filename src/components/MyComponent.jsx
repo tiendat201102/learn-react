fbaba45d04d7fe383aa5ad2e04a1104fa16fb86a
tiendat201102 <79494559+tiendat201102@users.noreply.github.com>
2024-08-06 10:49:07 +0700

    video 19

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -6,10 +6,21 @@ export default class MyComponent extends Component {
         name: "Dat",
         age: 22,
     }
+    handleClick() {
+        console.log("click me");
+    }
+
+    handleOnMouseOver(event) {
+        console.log(event);
+        
+    }
+
   render() {
     return (
       <div>
         my name is {this.state.name}
+        <button onClick={this.handleClick}>Click me</button>
+        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
       </div>
     )
   }

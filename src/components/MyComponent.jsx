c4d165a88155eeb9f6a9c3ff3f7e501e272836e0
tiendat201102 <79494559+tiendat201102@users.noreply.github.com>
2024-08-07 08:35:44 +0700

    video 21

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,33 +1,49 @@
-
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class MyComponent extends Component {
-    state = {
-        name: "Dat",
-        age: 22,
-    }
+  state = {
+    name: "Dat",
+    age: 22,
+  };
+
+  handleClick = (event) => {
+    console.log("click me");
+    console.log("My name is", this.state.name);
+    this.setState({
+      name: "Tien Dat",
+    });
+  };
+
+  handleOnChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
     
-    handleClick = (event) => {
-        console.log("click me");
-        console.log("My name is", this.state.name);
-        this.setState({
-            name: "Tien Dat"
-        });
-    }
-
-    handleOnMouseOver(event) {
-        console.log(event);
-        
-    }
-
+  };
   render() {
     return (
       <div>
         my name is {this.state.name}
-        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
         <button onClick={this.handleClick}>Click me</button>
+        <form
+          onSubmit={(event) => {
+            this.handleOnSubmit(event);
+          }}
+        >
+          <input
+            type="text"
+            onChange={(event) => {
+              this.handleOnChange(event);
+            }}
+          />
+          <button>submit</button>
+        </form>
       </div>
-    )
+    );
   }
 }
-

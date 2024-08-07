f3bf17d745778b8063c8118f0ed9a67e8a353fd0
tiendat201102 <79494559+tiendat201102@users.noreply.github.com>
2024-08-07 10:11:07 +0700

    video 24

--- src/components/DisplayInfor.jsx
+++ src/components/DisplayInfor.jsx
@@ -1,14 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class DisplayInfor extends Component {
   render() {
-    const {name, age} = this.props;
-    
+    const { listUsers } = this.props;
+    console.log(listUsers);
+
     return (
       <div>
-        <div>My name is {name}</div>
+        {/* <div>My name is {name}</div>
         <div>My age is {age}</div>
+        <hr /> */}
+        {listUsers.map((user) => {
+          return (
+            <div key={user.id}>
+              <div>My name is {user.name} </div>
+              <div>My age is {user.age} </div>
+              <hr />
+            </div>
+          );
+        })}
       </div>
-    )
+    );
   }
 }
--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -3,13 +3,26 @@ import UserInfor from "./UserInfor";
 import DisplayInfor from "./DisplayInfor";
 
 export default class MyComponent extends Component {
-  
+  state = {
+    listUsers: [
+      { id: 1, name: "Tien Dat", age: "30" },
+      { id: 2, name: "Nguyen Van", age: "25" },
+      { id: 3, name: "Tran Thi", age: "28" },
+      { id: 4, name: "Le Hoang", age: "33" },
+      { id: 5, name: "Pham Minh", age: "22" },
+      { id: 6, name: "Vo Thanh", age: "37" },
+      { id: 7, name: "Dang Quoc", age: "31" },
+      { id: 8, name: "Bui Thuy", age: "29" },
+      { id: 9, name: "Hoang Anh", age: "26" },
+      { id: 10, name: "Duong Mai", age: "34" }
+    ]
+  }
+
   render() {
-    const myInfor = ["a","b","c"];
     return (
       <div>
         <UserInfor />
-        <DisplayInfor name="Tien Dat" age="30" myInfor={myInfor}/>
+        <DisplayInfor listUsers={this.state.listUsers}/>
       </div>
     );
   }

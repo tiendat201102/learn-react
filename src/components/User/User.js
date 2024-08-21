01c671a036b5f80c37be1772154586e4f945f2ab
tiendat201102 <79494559+tiendat201102@users.noreply.github.com>
2024-08-21 09:31:28 +0700

    video 48

---
+++ src/components/User/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Header/Header'
+
+export default function User(props) {
+  return (
+    <div>
+        <Header/>
+      User component
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from "./components/Admin/Admin";
+import User from "./components/User/User";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     {/* <React.StrictMode> */}
-      <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="users" element={<User />} />
+        <Route path="admins" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
     {/* </React.StrictMode> */}
   </Provider>
 );

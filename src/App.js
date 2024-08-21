bfeddc7039a6f1d9e958ca1fdfb055086606ca14
tiendat201102 <79494559+tiendat201102@users.noreply.github.com>
2024-08-21 08:32:21 +0700

    video 46

--- src/App.js
+++ src/App.js
@@ -3,40 +3,18 @@ import "./App.scss";
 import { useDispatch, useSelector } from "react-redux";
 // import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from "./components/MyComponent";
-    
+import 'bootstrap/dist/css/bootstrap.css';
 import React, { Component } from 'react'
 import RiceLandingPage from "./components/Giaodien";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app-container">
-        <MyComponent />
-        {/* <RiceLandingPage/> */}
-      </div>
-    )
-  }
+
+const App = () => {
+
+  return (
+    <div className="App">
+      Hello world
+      <button>test</button>
+    </div>
+  );
 }
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit .
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//     <div>
-//       <MyComponent/>
-//     </div>
-//   );
-// }
 export default App;

ed0c77e6293a719e685950cf81c0896d1c19770c
tiendat201102 <79494559+tiendat201102@users.noreply.github.com>
2024-09-05 09:11:46 +0700

    video 54

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import Header from '../Header/Header'
+import React, { useState } from "react";
+import Header from "../Header/Header";
+import SideBar from "./SideBar";
+import "./Admin.scss"
+import {
+  FaBars,
+  FaHeart,
+} from "react-icons/fa";
 
 export default function Admin(props) {
+  const [collapsed, setCollapsed] = useState(false);
   return (
-    <div>
-        {/* <Header/> */}
-      Admin component
+    <div className="admin-container">
+      <div className="admin-sidebar">
+        <SideBar collapsed={collapsed}/>
+      </div>
+      <div className="admin-content">
+        <FaBars onClick={() => {setCollapsed(!collapsed)}}/>
+        content here
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/components/Admin/SideBar.js
@@ -0,0 +1,104 @@
+import React from "react";
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarHeader,
+  SidebarContent,
+  SidebarFooter,
+} from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import {
+  FaTachometerAlt,
+  FaGem,
+  FaList,
+  FaGithub,
+  FaRegLaughWink,
+  FaHeart,
+} from "react-icons/fa";
+
+
+import sidebarBg from "../../assests/bg2.jpg";
+
+export default function SideBar({
+  image,
+  collapsed,
+  toggled,
+  handleToggleSidebar,
+}) {
+  return (
+    <ProSidebar
+      image={sidebarBg}
+      collapsed={collapsed}
+      toggled={toggled}
+      breakPoint="md"
+      onToggle={handleToggleSidebar}
+    >
+      <SidebarHeader>
+        <div
+          style={{
+            padding: "24px",
+            textTransform: "uppercase",
+            fontWeight: "bold",
+            fontSize: 14,
+            letterSpacing: "1px",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+          }}
+        >
+          Tien Dat
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <Menu iconShape="circle">
+          <MenuItem
+            icon={<FaTachometerAlt />}
+            suffix={<span className="badge red">NEW</span>}
+          >
+            dashboard
+          </MenuItem>
+          <MenuItem icon={<FaGem />}>components</MenuItem>
+        </Menu>
+        <Menu iconShape="circle">
+          <SubMenu
+            title="Submenu"
+            suffix={<span className="badge yellow">3</span>}
+            icon={<FaRegLaughWink />}
+          >
+            <MenuItem>1</MenuItem>
+            <MenuItem>2</MenuItem>
+            <MenuItem>3</MenuItem>
+          </SubMenu>
+        </Menu>
+      </SidebarContent>
+      <SidebarFooter style={{ textAlign: "center" }}>
+        <div
+          className="sidebar-btn-wrapper"
+          style={{
+            padding: "20px 24px",
+          }}
+        >
+          <a
+            href="https://github.com/azouaoui-med/react-pro-sidebar"
+            target="_blank"
+            className="sidebar-btn"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+            <span
+              style={{
+                whiteSpace: "nowrap",
+                textOverflow: "ellipsis",
+                overflow: "hidden",
+              }}
+            >
+              viewSource
+            </span>
+          </a>
+        </div>
+      </SidebarFooter>
+    </ProSidebar>
+  );
+}
